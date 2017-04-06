49c0f0356d6fd53daa30039ae0b3e7091ecab72d
bmiller52 <millerbam52@gmail.com>
2017-04-06 10:24:58 -0400

    Update

--- js/scripts_cal.js
+++ js/scripts_cal.js
@@ -1,27 +1,27 @@
 var first_num = prompt('Pick a number');
 var user_num = parseInt(first_num);
 
+var pick_symbol = prompt('Type a mathematical symbol + - * /');
+
 var second_num = prompt('Pick another number');
 var user_two_num = parseInt(second_num);
 
-var pick_symbol = prompt('Pick a mathematical symbol + - * /');
-
-if (pick_symbol == '+') {
+if (pick_symbol === '+') {
   var the_math = user_num + user_two_num;
   document.write(user_num + ' + ' + user_two_num + ' = ' + the_math);
 }
 
-if (pick_symbol == '-') {
+if (pick_symbol === '-') {
   var the_math = user_num - user_two_num;
   document.write(user_num + ' - ' + user_two_num + ' = ' + the_math);
 }
 
-if (pick_symbol == '*') {
+if (pick_symbol === '*') {
   var the_math = user_num * user_two_num;
   document.write(user_num + ' * ' + user_two_num + ' = ' + the_math);
 }
 
-if (pick_symbol == '/') {
+if (pick_symbol === '/') {
   var the_math = user_num / user_two_num;
   document.write(user_num + ' / ' + user_two_num + ' = ' + the_math);
 }
